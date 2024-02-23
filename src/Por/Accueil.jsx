6316f26dded4a890c028dc787b2f5ac64a93e9c2
reda12323 <redalextayzi@gmail.com>
2024-02-23 15:37:29 +0100

    new modification executer

--- src/Por/Accueil.jsx
+++ src/Por/Accueil.jsx
@@ -64,10 +64,13 @@ export default function Accueil(props) {
                             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                         </svg>
                         </button>
-                        <div id="card">   
+                        <div>
+                            <div id="Panier">Panier</div>
+                            <div id="vide">(vide)</div>
+                        </div>
                             <img src={Imgpanier} id="ImgP" alt=""/>
                             <div className="nav-cart-count">0</div>
-                        </div>
+                        
                     </form>
                     <SearchResultsListe className="my-2" id="SearchRL" results={props.results} />
                     
